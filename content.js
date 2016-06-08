19ea5f7f065f1a7a6c094414345d8bb5a685f11c
Alan <aboutuser@ABT-AI-H03467.local>
2016-06-08 15:05:30 -0400

    Add some to-dos.

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
   console.log(response.farewell);
 });
+
+/*
+	Attach listener to radios $( "[type=radio]" )
+	On select:
+	 $.get to taxene children
+	 write the high / median / low weights -- maybe as placeholder text?
+*/
